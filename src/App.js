e4ca5275d638dd816fa11dd52f7fdf7c02bba28a
WonderliP <floyd.wonderlip@gmail.com>
2023-11-24 20:27:05 +0200

    Add React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+(function () {
+  const root = document.createElement('div');
+  root.id = 'root';
+  document.body.prepend(root);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const babel = document.createElement('script');
+  babel.src = 'https://unpkg.com/@babel/standalone/babel.min.js';
 
-export default App;
+  babel.onload = function () {
+    const react = document.createElement('script');
+    react.src = 'https://unpkg.com/react@18/umd/react.development.js';
+
+    react.onload = function () {
+      const reactDOM = document.createElement('script');
+      reactDOM.src =
+        'https://unpkg.com/react-dom@18/umd/react-dom.development.js';
+
+      reactDOM.onload = function () {
+        const myScript = document.createElement('script');
+        myScript.type = 'text/babel';
+        myScript.setAttribute('data-presets', 'react');
+        myScript.setAttribute('data-plugins', 'transform-react-jsx');
+        myScript.innerHTML = `
+          const root = ReactDOM.createRoot(document.getElementById('root'));
+          root.render(
+            <React.StrictMode>
+            <App />
+            </React.StrictMode>
+            );
+            
+            function App() {
+              return <h1>Hello, React!</h1>
+            }
+        `;
+        document.body.append(myScript);
+      };
+      document.body.append(reactDOM);
+    };
+    document.body.append(react);
+  };
+  document.body.append(babel);
+})();
